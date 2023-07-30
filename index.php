a0a4fb92f4eb069261cc578358e36515a99ecab8
mulon <mulophoto@gmail.com>
2023-07-30 22:28:49 +0200

    created index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <h1>
+
+        <?php
+        $user = 'user';
+        echo '<h1> hi </h1>';
+        echo  "hello {$user}";    
+        echo '</br>Good Day ' . $user . '1';
+        ?></h1>
+    <p>My first paragraph.</p>
+
+</body>
+
+</html>
