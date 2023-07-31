10003b72bb8e56d15766b1e5cb9250aaa01f7ace
mulo <mulogame123@gmail.com>
2023-07-31 12:32:55 +0200

    Added form

--- index.php
+++ index.php
@@ -3,15 +3,25 @@
 
 <body>
 
-    <h1>
 
-        <?php
-        $user = 'user';
-        echo '<h1> hi </h1>';
-        echo  "hello {$user}";    
-        echo '</br>Good Day ' . $user . '1';
-        ?></h1>
-    <p>My first paragraph.</p>
+   
+
+        <!-- <?php
+                $user = 'Login';
+                echo '<h1> hi </h1>';
+                echo  "hello {$user}";
+                echo '</br>Good Day ' . $user . '1';
+                ?></h1> -->
+        <p>LOGIN</p>
+
+        <form  action="/action_page.php">
+            <label for="username">Username</label><br>
+            <input type="text" id="fname" name="username"><br>
+            <label for="password">Password</label><br>
+            <input type="text" id="lname" name="password">
+            <input type="submit" value="Submit">
+        </form>
+
 
 </body>
 
